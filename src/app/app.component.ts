749a8f24ea2bc852213136ecd1c2cf3a6ebaa29c
María Imbert <imbert.maria@hotmail.com>
2021-07-26 16:16:03 -0300

    Inicio curso de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+//DECORADOR
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //SELECTOR QUE LO CARGA
+  templateUrl: './app.component.html', //TEMPLATE QUE SE CARGA
+  styleUrls: ['./app.component.css'] //STYLES 
 })
+
+//CLASE
 export class AppComponent {
   title = 'miprimeraApp';
+  saludo = "Hola Alumnos de Angular";
 }
---
+++ src/app/empleados/empleados.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+
+//creo el decorador
+@Component({
+    selector: "app-empleados",
+    templateUrl: "./empleados.component.html",
+    styleUrls: ['./app.empleados.css']
+})
+
+//Creo la clase
+export class EmpleadosComponent {
+
+}
